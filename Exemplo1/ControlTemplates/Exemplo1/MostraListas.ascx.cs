f7c42478f36000b8343b4381c070a6f4283d48c4
Edvaldo Barbosa Guimarães <edvaldoguimaraes@multconnect.com.br>
2021-03-01 05:40:36 -0300

    ajustes

--- Exemplo1/ControlTemplates/Exemplo1/MostraListas.ascx.cs
+++ Exemplo1/ControlTemplates/Exemplo1/MostraListas.ascx.cs
@@ -15,10 +15,10 @@ namespace Exemplo1.ControlTemplates.Exemplos
             foreach (SPList list in SPContext.Current.Web.Lists)
             {
                 string htmlTemplate = "<div class=\"card t-4 rounded \" style=\"float:left; height:500px;margin-right:10px;margin-bottom:10px;min-width:30%;width:30%;padding:0px\">" +
-               "<img class=\"card-img-top\" src=\"" + list.ImageUrl + "\" alt=\"Card image cap\" style=\"margin:auto;height:200px\">" +
+               "<img class=\"card-img-top\" src=\"" + list.ImageUrl + "\" alt=\"Card image cap\" style=\"margin:auto;\">" +
                "<div class=\"card-body\">" +
                "<h5 class=\"card-title\">" +list.Title + "</h5>" +
-               "<p class=\"card-text\" style=\"min-height:60%\">" + list.Description + "</p>" +
+               "<p class=\"card-text\" style=\"min-height:40%\">" + list.Description + "</p>" +
                "<a  href = \"" +list.DefaultViewUrl + "\" class=\"btn btn-danger  btn-lg\">Ver Lista" +
                "<a  href = \"http://shplab/_layouts/15/listedit.aspx?List={" + list.ID + "}\" class=\"btn btn-danger  btn-lg mt-2\">Administrar Lista" +
                "</a>" +
