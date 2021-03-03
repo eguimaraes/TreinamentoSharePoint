11d051c57c458338c9bf3ae99cb0e0e2bd40d188
Edvaldo Barbosa Guimarães <edvaldoguimaraes@multconnect.com.br>
2021-03-03 14:26:06 -0300

    Adicionado Robot Basico Parametrizado

---
+++ RobotListas/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SharePoint;
+
+namespace RobotListas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (SPSite site = new SPSite(args[0]))
+            {
+
+                using (SPWeb web = site.AllWebs[args[1]])
+                {
+
+                    SPListCollection listas = web.Lists;
+
+                    foreach (SPList lista in listas)
+                    {
+
+
+                        Console.WriteLine(lista.Title);
+
+
+                    }
+
+
+
+                }
+
+
+
+            }
+
+        }
+
+
+    }
+}
