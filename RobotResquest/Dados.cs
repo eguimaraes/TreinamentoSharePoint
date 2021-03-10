fa91f634e9135e9f7dfc01452c58a91182c1a6be
Edvaldo Barbosa Guimarães <edvaldoguimaraes@multconnect.com.br>
2021-03-10 18:29:04 -0300

    Adicionados Robots Lista e Request

---
+++ RobotResquest/Dados.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotResquest
+{
+    public class Dados
+    {
+        public string name;
+        public string CPF00N1I00000LrNdg;
+        public string email;
+        public string phone;
+        public string type;
+        public string description;
+        public string origin;
+
+        public Dados(string name,string CPF00N1I00000LrNdg, string email, string phone, string type, string  description, string origin)
+        {
+            this.name = name;
+            this.CPF00N1I00000LrNdg = CPF00N1I00000LrNdg;
+            this.email = email;
+            this.phone = phone;
+            this.type = type;
+            this.description = description;
+            this.origin = origin;
+
+            
+
+        }
+
+
+
+        public Dados() { }
+
+
+
+
+        }
+
+    }
+
