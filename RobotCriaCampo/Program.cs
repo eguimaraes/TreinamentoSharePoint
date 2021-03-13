557c2e6d2eed8711954a41147c76712af36e4980
Edvaldo Barbosa Guimarães <edvaldoguimaraes@multconnect.com.br>
2021-03-13 11:50:21 -0300

    robot cria campos adiconado

--- RobotCriaCampo/Program.cs
+++ RobotCriaCampo/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.SharePoint;
 
 namespace RobotCriaCampo
 {
@@ -10,6 +11,32 @@ namespace RobotCriaCampo
     {
         static void Main(string[] args)
         {
+            
+            using (SPSite site=new SPSite(args[0]))
+            {
+
+                foreach (SPWeb web in site.AllWebs)
+                {
+
+
+                   Guid listId= web.Lists.Add(args[1], args[2], SPListTemplateType.GenericList);
+
+                    web.Update();
+
+                    SPList lista = web.Lists[listId];
+
+                    lista.Fields.AddFieldAsXml(args[3]);
+
+                    lista.Update();
+
+
+                }
+
+
+
+            }
+
+
         }
     }
 }
